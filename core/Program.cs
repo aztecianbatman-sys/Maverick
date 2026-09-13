1646964fce03ce137966668e6c719531f46cb648
Fulltrack <aztecianbatman@gmail.com>
2026-09-13 21:04:15 +0530

    feat: register Maverick Phase 4 protection services

--- core/Program.cs
+++ core/Program.cs
@@ -8,15 +8,26 @@ public static class Program
     public static Task Main(string[] args)
     {
         var builder = Host.CreateApplicationBuilder(args);
-        builder.Services.AddWindowsService(options => options.ServiceName = "Maverick Core");
+
+        builder.Services.AddWindowsService(options =>
+            options.ServiceName = "Maverick Core");
+
         builder.Services.AddSingleton<CorePaths>();
         builder.Services.AddSingleton<Journal>();
         builder.Services.AddSingleton<FileInspector>();
         builder.Services.AddSingleton<SystemInventory>();
-        builder.Services.AddSingleton<QuarantineMetadata>();
+        builder.Services.AddSingleton<ProtectionAnalyzer>();
+        builder.Services.AddSingleton<QuarantineService>();
+        builder.Services.AddSingleton<ProtectionService>();
         builder.Services.AddSingleton<ScanService>();
-        builder.Services.AddHostedService<SecurityMonitor>();
+
+        builder.Services.AddSingleton<SecurityMonitor>();
+        builder.Services.AddHostedService(sp =>
+            sp.GetRequiredService<SecurityMonitor>());
+
+        builder.Services.AddHostedService<ProcessMonitor>();
         builder.Services.AddHostedService<PipeServer>();
+
         return builder.Build().RunAsync();
     }
 }
