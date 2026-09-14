5de47e714dbb32fbe7b858ef466a898dd7a12b52
Fulltrack <aztecianbatman@gmail.com>
2026-09-14 09:12:35 +0530

    feat: expose safe native folder picker

--- electron/preload.js
+++ electron/preload.js
@@ -9,6 +9,7 @@ contextBridge.exposeInMainWorld("maverick", {
   testApi: (input) => ipcRenderer.invoke("api:test", input),
   sendChat: (input) => ipcRenderer.invoke("chat:send", input),
   coreRequest: (command, payload) => ipcRenderer.invoke("core:request", command, payload),
+  pickFolder: () => ipcRenderer.invoke("core:pick-folder"),
   configureDefaultMonitoring: () => ipcRenderer.invoke("core:configure-default-monitoring"),
   openExternal: (url) => ipcRenderer.invoke("shell:open-external", url),
 });
